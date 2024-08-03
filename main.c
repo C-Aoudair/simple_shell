71d798467628c73d1b88b2251f7ef5109cebc9ca
C-Aoudair <aoudair16031999@gmail.com>
2024-08-03 17:23:48 +0100

    adjust style of the main.c

--- main.c
+++ main.c
@@ -5,35 +5,45 @@
 #include <sys/types.h>
 
 /**
- * The main function of simple shlell program
- * 
- * @return 0
+ * main - simple shell
+ * @argc: argument count
+ * @argv: argument vector
+ * @envp: environment variables
+ * Return: 0
  */
-int main(int argc, char *argv[], char *envp[]) {
-    char *line = NULL;
-    int *status;
-    size_t len = 0;
-    ssize_t read;
-    while (1) {
-        printf("simple-shell> ");
-        read = getline(&line, &len, stdin);
-        if (read == -1) {
-            break;
-        }
-        line[read - 1] = '\0';
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork");
-            exit(1);
-        }
-        if (pid == 0) {
-            execve(line, argv, envp);
-            perror("simple-shell");
-            exit(0);
-        }
-        wait(status);    
-    }
-    free(line);
-    return 0;
-    
+
+int main(int argc __attribute__((unused)), char *argv[], char *envp[])
+{
+	char *line = NULL;
+	int *status = NULL;
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+
+	while (1)
+	{
+		printf("simple-shell> ");
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			break;
+		}
+		line[read - 1] = '\0';
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(1);
+		}
+		if (pid == 0)
+		{
+			execve(line, argv, envp);
+			perror("simple-shell");
+			exit(0);
+		}
+		wait(status);
+	}
+
+	free(line);
+	return (0);
 }
